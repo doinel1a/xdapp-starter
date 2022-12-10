7d9369fdb17753d4f90200604f5dbe379fe37333
doinel1a <doinel1atanasiu@gmail.com>
2022-12-11 00:22:26 +0100

    Login / Logout logic implemented

--- dapp/src/components/layout/navbar.tsx
+++ dapp/src/components/layout/navbar.tsx
@@ -1,43 +1,46 @@
 import { useGetIsLoggedIn } from '@elrondnetwork/dapp-core/hooks';
+import { logout } from '@elrondnetwork/dapp-core/utils';
 import { faLock, faUnlock } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { routeNames } from '../../config/routes';
 
 export default function Navbar() {
 	const isLoggedIn = useGetIsLoggedIn();
 
 	return (
 		<nav className='w-full flex items-center px-8 py-4 bg-secondary'>
-			<a title='Home' href='HOME URL' className='text-2xl mr-auto'>
-				<span className='transition-colors hover:text-accent-primary focus:text-accent-primary'>
-					xDapp
-				</span>{' '}
-				—{' '}
-				<span className='transition-colors hover:text-accent-primary focus:text-accent-primary'>
-					Starter
-				</span>
-			</a>
-			<a
-				title='Login with your wallet'
-				href='UNLOCK URL'
-				{...{
-					target: '_blank'
-				}}
-				className='px-3 py-2 rounded-lg bg-accent-primary
-        transition-colors hover:bg-accent-primary-state focus:bg-accent-primary-state'
+			<Link
+				title='Home'
+				to={routeNames.home}
+				className='mr-auto text-2xl transition-colors hover:text-accent-primary focus:text-accent-primary'
 			>
-				{!isLoggedIn ? (
-					<>
-						Login &nbsp;
-						<FontAwesomeIcon icon={faUnlock} />
-					</>
-				) : (
-					<>
-						Logout &nbsp;
-						<FontAwesomeIcon icon={faLock} />
-					</>
-				)}
-			</a>
+				xDapp Starter
+			</Link>
+			{!isLoggedIn ? (
+				<Link
+					title='Login with your wallet'
+					to={routeNames.login}
+					className='px-3 py-2 rounded-lg bg-accent-primary
+								transition-colors hover:bg-accent-primary-state focus:bg-accent-primary-state'
+				>
+					Login &nbsp;
+					<FontAwesomeIcon icon={faUnlock} />
+				</Link>
+			) : (
+				<button
+					type='button'
+					title='Logout dapp'
+					className='px-3 py-2 rounded-lg bg-accent-primary
+				transition-colors hover:bg-accent-primary-state focus:bg-accent-primary-state'
+					onClick={() => logout()}
+				>
+					Logout &nbsp;
+					<FontAwesomeIcon icon={faLock} />
+				</button>
+			)}
 		</nav>
 	);
 }
--- dapp/src/pages/login.tsx
+++ dapp/src/pages/login.tsx
@@ -1,13 +1,27 @@
+import { useWebWalletLogin } from '@elrondnetwork/dapp-core/hooks/login/useWebWalletLogin';
 import React from 'react';
 
 import AuthRedirectWrapper from '../components/helpers/auth-redirect-wrapper';
 import PageWithTitle from '../components/helpers/page-with-title';
+import { routeNames } from '../config/routes';
 
 function LoginPage() {
+	const [initiateLogin] = useWebWalletLogin({
+		callbackRoute: routeNames.dashboard
+	});
+
 	return (
-		<div>
-			<h1>Login</h1>
-		</div>
+		<section className='w-96 h-72 container flex flex-col p-10 rounded-xl border border-tertiary bg-secondary'>
+			<h1 className='text-2xl'>Login</h1>
+			<h2 className='text-xl'>Connect to a wallet:</h2>
+			<button
+				className='mt-auto px-3 py-2 rounded-lg bg-accent-primary
+								transition-colors hover:bg-accent-primary-state focus:bg-accent-primary-state'
+				onClick={() => initiateLogin()}
+			>
+				Web wallet
+			</button>
+		</section>
 	);
 }
 
